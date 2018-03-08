010f89e8c6956744ae7efe0bc4ff08e71b700ac5
Patryk Kaczmarek <patryk.kaczmarek996@gmail.com>
2018-03-08 22:50:58 +0100

    Reading done!

--- WaveHeader.cpp
+++ WaveHeader.cpp
@@ -2,23 +2,25 @@
 #include <iostream>
 
 void WaveHeaderReader::Read() {
-    ReadXBitToString(header_.chunck_id_, 4);
-    file_.read(reinterpret_cast<char* >(&header_.chunck_size_), sizeof(header_.chunck_size_));
+    ReadXBitToString(header_.chunk_id_, 4);
+    file_.read(reinterpret_cast<char* >(&header_.chunk_size_), sizeof(header_.chunk_size_));
     ReadXBitToString(header_.format_, 4);
     ReadXBitToString(header_.sub_chank1_id_, 4);
-    file_.read(reinterpret_cast<char* >(&header_.chunck1_size_), sizeof(uint32_t));
+    file_.read(reinterpret_cast<char* >(&header_.chunk1_size_), sizeof(uint32_t));
     file_.read(reinterpret_cast<char* >(&header_.format_code_), sizeof(uint16_t));
     file_.read(reinterpret_cast<char* >(&header_.number_of_channels_), sizeof(uint16_t));
     file_.read(reinterpret_cast<char* >(&header_.samples_per_second_), sizeof(uint32_t));
     file_.read(reinterpret_cast<char* >(&header_.bytes_per_second_), sizeof(uint32_t));
     file_.read(reinterpret_cast<char* >(&header_.bytes_per_sample_frame_), sizeof(uint16_t));
     file_.read(reinterpret_cast<char* >(&header_.bits_per_sample_), sizeof(uint16_t));
+    ReadXBitToString(header_.sub_chank2_id_, 4);
+    file_.read(reinterpret_cast<char* >(&header_.sub_chank2_size_), sizeof(uint32_t));
 
 }
 
 void WaveHeaderReader::PrintInfo() {
-    std::cout << "ChunckID: "<<header_.chunck_id_
-              <<"\nChunck size: "<<header_.chunck_size_
+    std::cout << "ChunckID: "<<header_.chunk_id_
+              <<"\nChunck size: "<<header_.chunk_size_
               <<"\nFormat: "<<header_.format_
               <<"\nSubChunck1ID: "<<header_.sub_chank1_id_
               <<"\nFormat Code: "<<header_.format_code_
@@ -26,7 +28,10 @@ void WaveHeaderReader::PrintInfo() {
               <<"\nSamples per second: "<<header_.samples_per_second_
               <<"\nBytes per second: "<<header_.bytes_per_second_
               <<"\nBytes per sample frame: "<<header_.bytes_per_sample_frame_
-              <<"\nBits per sample: "<<header_.bits_per_sample_;
+              <<"\nBits per sample: "<<header_.bits_per_sample_
+              <<"\nSubChank2ID: "<<header_.sub_chank2_id_
+              <<"\nSubChank2Size: "<<header_.sub_chank2_size_;
+    std::cout<<"\nSamples: "<<((8*header_.sub_chank2_size_)/(header_.number_of_channels_*header_.bits_per_sample_))<<"\n";
 }
 
 void WaveHeaderReader::ReadXBitToString(std::string& str, const size_t X) {
--- WaveHeader.hpp
+++ WaveHeader.hpp
@@ -4,17 +4,19 @@
 #include <cstdint>
 
 struct WaveHeader {
-    std::string chunck_id_;
-    uint32_t chunck_size_;
+    std::string chunk_id_;
+    uint32_t chunk_size_;
     std::string format_;
     std::string sub_chank1_id_;
-    uint32_t chunck1_size_;
+    uint32_t chunk1_size_;
     uint16_t format_code_;
     uint16_t number_of_channels_;
     uint32_t samples_per_second_;
     uint32_t bytes_per_second_;
     uint16_t bytes_per_sample_frame_;
     uint16_t bits_per_sample_;
+    std::string sub_chank2_id_;
+    uint32_t sub_chank2_size_;
 };
 
 
