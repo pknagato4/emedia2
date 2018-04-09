ce5108fbc99106922b22115aeb67f360b0c0d8c7
Patryk Kaczmarek <patryk.kaczmarek996@gmail.com>
2018-04-09 22:59:16 +0200

    gnuplot show plots inside app

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include "WaveHeader.hpp"
 #include <iostream>
+
+#include "gnuplot-cpp-interface/GNUPlot/GNUPlot.h"
+
 int main(int argc, char** argv) {
     std::string filename(argv[1]);
     WaveReader wav(filename);
     wav.Read();
     wav.PrintInfo();
+
+    std::vector<std::string> script;
+    script.push_back("set terminal x11");
+    script.push_back("reset");
+    script.push_back("plot \"sample1fft.txt\" with lines");
+
+    GNUPlot plotter;
+    plotter.open();
+    plotter.execute(script);
+    plotter.execute(script);
+
+    getchar(); // prevent graph to close
+
+    plotter.write("exit");
+    plotter.flush();
+    plotter.close();
+
 }
